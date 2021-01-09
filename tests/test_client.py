434009ca38902ad7380e634885744d8650e7960c
markallanson <mark@allanson.org>
2021-01-09 20:26:38 +0000

    Rework tests to use pytest features, specifically to make use of pytest_aio

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,8 @@
 import re
-from asyncio import get_event_loop
 from http import HTTPStatus
 from unittest import TestCase
 
+import pytest
 from aioresponses import aioresponses
 
 from octopus_energy import (
@@ -29,46 +29,50 @@ class ClientTest(TestCase):
         async with OctopusEnergyRestClient(_MOCK_TOKEN) as client:
             return await client.get_gas_consumption_v1("mprn", "serial_number")
 
+    @pytest.mark.asyncio
     @aioresponses()
-    def test_raises_api_auth_error_when_authentication_fails(self, aiomock: aioresponses):
+    async def test_raises_api_auth_error_when_authentication_fails(self, mock_aioresponses):
         with self.subTest("elec consumption v1"):
             with self.assertRaises(ApiAuthenticationError):
-                aiomock.get(re.compile(".*"), status=HTTPStatus.UNAUTHORIZED.value)
-                get_event_loop().run_until_complete(self.get_electricity_consumption_v1())
+                mock_aioresponses.get(re.compile(".*"), status=HTTPStatus.UNAUTHORIZED.value)
+                await self.get_electricity_consumption_v1()
 
         with self.subTest("gas consumption v1"):
             with self.assertRaises(ApiAuthenticationError):
-                aiomock.get(re.compile(".*"), status=HTTPStatus.UNAUTHORIZED.value)
-                get_event_loop().run_until_complete(self.get_gas_consumption_v1())
+                mock_aioresponses.get(re.compile(".*"), status=HTTPStatus.UNAUTHORIZED.value)
+                await self.get_gas_consumption_v1()
 
+    @pytest.mark.asyncio
     @aioresponses()
-    def test_raises_api_error_when_not_ok(self, aiomock: aioresponses):
+    async def test_raises_api_error_when_not_ok(self, aiomock: aioresponses):
         with self.subTest("elec consumption v1"):
             with self.assertRaises(ApiError):
                 aiomock.get(re.compile(".*"), status=HTTPStatus.INTERNAL_SERVER_ERROR.value)
-                get_event_loop().run_until_complete(self.get_electricity_consumption_v1())
+                await self.get_electricity_consumption_v1()
 
         with self.subTest("gas consumption v1"):
             with self.assertRaises(ApiError):
                 aiomock.get(re.compile(".*"), status=HTTPStatus.INTERNAL_SERVER_ERROR.value)
-                get_event_loop().run_until_complete(self.get_gas_consumption_v1())
+                await self.get_gas_consumption_v1()
 
+    @pytest.mark.asyncio
     @aioresponses()
-    def test_raises_not_found_api_error_when_not_ok(self, aiomock: aioresponses):
+    async def test_raises_not_found_api_error_when_not_ok(self, aiomock: aioresponses):
         with self.subTest("elec consumption v1"):
             with self.assertRaises(ApiNotFoundError):
                 aiomock.get(re.compile(".*"), status=HTTPStatus.NOT_FOUND.value)
-                get_event_loop().run_until_complete(self.get_electricity_consumption_v1())
+                await self.get_electricity_consumption_v1()
 
         with self.subTest("gas consumption v1"):
             with self.assertRaises(ApiNotFoundError):
                 aiomock.get(re.compile(".*"), status=HTTPStatus.NOT_FOUND.value)
-                get_event_loop().run_until_complete(self.get_gas_consumption_v1())
+                await self.get_gas_consumption_v1()
 
+    @pytest.mark.asyncio
     @aioresponses()
-    def test_get_elec_consumption_v1(self, aiomock: aioresponses):
+    async def test_get_elec_consumption_v1(self, aiomock: aioresponses):
         aiomock.get(re.compile(".*"), status=HTTPStatus.OK.value, payload={"results": []})
-        resp = get_event_loop().run_until_complete(self.get_electricity_consumption_v1())
+        resp = self.get_electricity_consumption_v1()
         with self.subTest("call made"):
             self.assertEqual(len(aiomock.requests), 1)
         with self.subTest("response returned"):
@@ -76,10 +80,11 @@ class ClientTest(TestCase):
         with self.subTest("response is dictionary"):
             self.assertIsNotNone(type(resp), dict)
 
+    @pytest.mark.asyncio
     @aioresponses()
-    def test_get_gas_consumption_v1(self, aiomock: aioresponses):
+    async def test_get_gas_consumption_v1(self, aiomock: aioresponses):
         aiomock.get(re.compile(".*"), status=HTTPStatus.OK.value, payload={"results": []})
-        resp = get_event_loop().run_until_complete(self.get_gas_consumption_v1())
+        resp = self.get_gas_consumption_v1()
         with self.subTest("call made"):
             self.assertEqual(len(aiomock.requests), 1)
         with self.subTest("response returned"):
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,7 +1,6 @@
-import asyncio
 from typing import Callable
-from unittest import TestCase
 
+import pytest
 from aioresponses import aioresponses
 
 from octopus_energy import OctopusEnergyRestClient
@@ -12,94 +11,117 @@ from tests import load_fixture_json
 _FAKE_API_TOKEN = "sk_live_xxxxxxxxxxxx"
 
 
-class E2eTests(TestCase):
-    def setUp(self) -> None:
-        super().setUp()
+@pytest.fixture
+def mock_aioresponses():
+    with aioresponses() as mock_aioresponses:
+        yield mock_aioresponses
 
-    @aioresponses()
-    def test_get_account_details_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_account_details("account_number")
 
-        self._run_test("v1/accounts/account_number", "account_response.json", get, aiomock)
+@pytest.mark.asyncio
+async def test_get_account_details_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_account_details("account_number")
 
-    @aioresponses()
-    def test_get_elec_consumption_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_electricity_consumption_v1("mpan", "serial_number")
+    await _run_get_test(
+        "v1/accounts/account_number", "account_response.json", get, mock_aioresponses
+    )
 
-        self._run_test(
-            "v1/electricity-meter-points/mpan/meters/serial_number/consumption",
-            "consumption_response.json",
-            get,
-            aiomock,
+
+@pytest.mark.asyncio
+async def test_get_elec_consumption_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_electricity_consumption_v1("mpan", "serial_number")
+
+    await _run_get_test(
+        "v1/electricity-meter-points/mpan/meters/serial_number/consumption",
+        "consumption_response.json",
+        get,
+        mock_aioresponses,
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_electricity_meter_points_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_electricity_meter_points_v1("mpan")
+
+    await _run_get_test(
+        "v1/electricity-meter-points/mpan",
+        "get_electricity_meter_points.json",
+        get,
+        mock_aioresponses,
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_gas_consumption_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_gas_consumption_v1("mprn", "serial_number")
+
+    await _run_get_test(
+        "v1/gas-meter-points/mprn/meters/serial_number/consumption",
+        "consumption_response.json",
+        get,
+        mock_aioresponses,
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_products_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_products_v1()
+
+    await _run_get_test("v1/products", "get_products_response.json", get, mock_aioresponses)
+
+
+@pytest.mark.asyncio
+async def test_get_product_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_product_v1("product_id")
+
+    await _run_get_test(
+        "v1/products/product_id", "get_product_response.json", get, mock_aioresponses
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_tariff_v1(mock_aioresponses: aioresponses):
+    async def get(client):
+        return await client.get_tariff_v1(
+            "product_id", EnergyTariffType.GAS, "tariff_code", RateType.STANDING_CHARGES
         )
 
-    @aioresponses()
-    def test_get_electricity_meter_points_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_electricity_meter_points_v1("mpan")
+    await _run_get_test(
+        "v1/products/product_id/gas-tariffs/tariff_code/standing-charges",
+        "get_tariff_response.json",
+        get,
+        mock_aioresponses,
+    )
 
-        self._run_test(
-            "v1/electricity-meter-points/mpan",
-            "get_electricity_meter_points.json",
-            get,
-            aiomock,
-        )
 
-    @aioresponses()
-    def test_get_gas_consumption_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_gas_consumption_v1("mprn", "serial_number")
+async def _run_get_test(
+    path: str, response_resource: str, func: Callable, mock_aioresponses: aioresponses
+):
+    mock_aioresponses.get(
+        f"{_API_BASE}/{path}",
+        payload=load_fixture_json(response_resource),
+    )
+    async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
+        response = await func(client)
+    assert response == load_fixture_json(response_resource)
 
-        self._run_test(
-            "v1/gas-meter-points/mprn/meters/serial_number/consumption",
-            "consumption_response.json",
-            get,
-            aiomock,
-        )
 
-    @aioresponses()
-    def test_get_products_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_products_v1()
-
-        self._run_test("v1/products", "get_products_response.json", get, aiomock)
-
-    @aioresponses()
-    def test_get_product_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_product_v1("product_id")
-
-        self._run_test("v1/products/product_id", "get_product_response.json", get, aiomock)
-
-    @aioresponses()
-    def test_get_tariff_v1(self, aiomock: aioresponses):
-        async def get():
-            async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
-                return await client.get_tariff_v1(
-                    "product_id", EnergyTariffType.GAS, "tariff_code", RateType.STANDING_CHARGES
-                )
-
-        self._run_test(
-            "v1/products/product_id/gas-tariffs/tariff_code/standing-charges",
-            "get_tariff_response.json",
-            get,
-            aiomock,
-        )
-
-    def _run_test(self, path: str, response_resource: str, func: Callable, aiomock: aioresponses):
-        aiomock.get(
-            f"{_API_BASE}/{path}",
-            payload=load_fixture_json(response_resource),
-        )
-        loop = asyncio.get_event_loop()
-
-        response = loop.run_until_complete(func())
-        self.assertEqual(response, load_fixture_json(response_resource))
+async def _run_post_test(
+    path: str,
+    request_resource: str,
+    response_resource: str,
+    func: Callable,
+    mock_aioresponses: aioresponses,
+):
+    mock_aioresponses.post(
+        f"{_API_BASE}/{path}",
+        payload=load_fixture_json(response_resource),
+    )
+    async with OctopusEnergyRestClient(_FAKE_API_TOKEN) as client:
+        response = await func(client)
+    assert response == load_fixture_json(response_resource)
